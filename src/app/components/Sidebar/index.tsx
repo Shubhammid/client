7cff13aee9ba3878278b8dea991c64a07fc81df6
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-16 16:29:43 +0530

    Added Logo and images in Public

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { LockIcon } from "lucide-react";
+import Image from "next/image";
 import React, { useState } from "react";
 
 const Sidebar = () => {
@@ -13,11 +15,31 @@ const Sidebar = () => {
   return (
     <div className={sidebarClassNames}>
       <div className="flex h-[100%] w-full flex-col justify-start">
+        
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="text-xl font-bold text-gray-800 dark:text-white">
             MIDLIST
           </div>
         </div>
+
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+            <Image
+            src="/logo.png"
+            alt="Logo"
+            width={40}
+            height={40}
+          />
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              MIDLIST TEAM
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
