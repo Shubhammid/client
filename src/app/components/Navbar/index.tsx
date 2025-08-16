ad42d28aa96ddee69c399d5682f2462b261c6962
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-16 15:45:24 +0530

    Started with Navbar

---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
+      <div className="flex items-center gap-8">Navbar</div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>DashboardWrapper</div>
-  )
-}
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+         <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
 
-export default DashboardWrapper
+export default DashboardWrapper;
