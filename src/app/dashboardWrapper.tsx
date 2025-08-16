fa5f50d59e25ba596319cdda7c71bb91c0efa076
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-16 15:23:51 +0530

    Setup is Done

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>DashboardWrapper</div>
+  )
+}
+
+export default DashboardWrapper
